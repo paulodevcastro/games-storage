cc7a17dbbcf9378648540e2436e6be783c2922ab
gil <guizits200@gmail.com>
2025-03-05 17:33:17 -0300

    start project

---
+++ games-storage-app/src/main/java/com/lit/games_storage/GamesStorageApplication.java
@@ -0,0 +1,13 @@
+package com.lit.games_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesStorageApplication.class, args);
+	}
+
+}
---
+++ games-storage-app/src/test/java/com/lit/games_storage/GamesStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lit.games_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
